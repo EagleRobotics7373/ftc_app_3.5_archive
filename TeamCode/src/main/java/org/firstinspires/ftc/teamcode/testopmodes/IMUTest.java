9151146f56550fd0ca77a253450a07f52d68f13b
Jordan Moss <kk4jrq@gmail.com>
2017-12-01 14:47:50 -0500

    Updates for pre-auto prep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testopmodes/IMUTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testopmodes/IMUTest.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.thirdmeetopmodes;
+package org.firstinspires.ftc.teamcode.testopmodes;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
@@ -47,15 +47,14 @@ import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
-import org.firstinspires.ftc.teamcode.eaglerobotics.library.Sensors.AdaIMU;
 import org.firstinspires.ftc.teamcode.eaglerobotics.library.drivetrain.Holonomic;
 
 /**
  * Demonstrates empty OpMode
  */
-@Autonomous(name = "Autonomous Meet 3", group = "Meet 3")
+@Autonomous(name = "IMU Test", group = "Test")
 //@Disabled
-public class Meet3Auto extends LinearOpMode{
+public class IMUTest extends LinearOpMode{
 
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -139,179 +138,17 @@ public class Meet3Auto extends LinearOpMode{
         leftIntake.setPosition(1);
         rightIntake.setPosition(0);
 
-        // Set up Vuforia
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parametersVu = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        parametersVu.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parametersVu);
+        waitForStart();
 
-        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
-
-        relicTrackables.activate();
-
-        // Set Team Color and Position while waiting for start
-        while (!isStarted()) {
-            // Set Red or Blue
-            if (gamepad1.x)
-                teamColor = Color.BLUE;
-            else if (gamepad1.b)
-                teamColor = Color.RED;
-
-             if (gamepad1.right_bumper) {
-             startingPosition = StartingPosition.RIGHT;
-             } else if (gamepad2.left_bumper) {
-             startingPosition = StartingPosition.LEFT;
-             }
-
-
-            telemetry.addData("Alliance Color: ", teamColor.toString());
-            if (teamColor == Color.NULL)
-                telemetry.addData("Boiiii: ", "Set the Colorrr");
-            if (startingPosition == StartingPosition.NULL) {
-                telemetry.addData("Fix it", " Pleaseee");
-            }
-
-            telemetry.update();
-        }
-
-        // Lower the Jewel Manipulator
-        jewelManipulator.setPosition(0);
-        sleep(3000);
-
-        // Go to each case for each color
-        // Check the Color
-        // Rotate Forward or Backwards based on color
-        if(teamColor == Color.BLUE){
-            if(colorSensorLeft.blue() > colorSensorRight.red() && colorSensorRight.red() > colorSensorLeft.blue()){
-                jewelRotator.setPosition(rightPosition);
-            } else if(colorSensorRight.blue() > colorSensorLeft.red() && colorSensorLeft.red() > colorSensorRight.blue()){
-                jewelRotator.setPosition(leftPosition);
-            }
-
-        } else if (teamColor == Color.RED) {
-            if(colorSensorLeft.blue() > colorSensorRight.red() && colorSensorRight.red() > colorSensorLeft.blue()){
-                jewelRotator.setPosition(leftPosition);
-            } else if(colorSensorRight.blue() > colorSensorLeft.red() && colorSensorLeft.red() > colorSensorRight.blue()){
-                jewelRotator.setPosition(rightPosition);
-            }
-        }
-
-        sleep(1000);
-        jewelRotator.setPosition(middlePosition);
-        sleep(1000);
-        jewelManipulator.setPosition(.5);
-
-        //Check the VuMark
-        //Attempt 3 times
-        for (int i = 0; i < 3 && opModeIsActive(); i++){
-            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-            if(vuMark != RelicRecoveryVuMark.UNKNOWN) {
-                visibleVuMark = vuMark;
-                break;
-            }
-            sleep(1000);
-        }
-        telemetry.addData("VuMark: ", visibleVuMark.toString());
-
-        // Grab that block
-        leftIntake.setPosition(.5);
-        rightIntake.setPosition(.5);
-
-        //Rasie the lift just barely
-        leftThreadedRodLift.setPower(1);
-        rightThreadedRodLift.setPower(1);
-        sleep(250);
-        leftThreadedRodLift.setPower(0);
-        rightThreadedRodLift.setPower(0);
-        sleep(500);
-
-        //Drive Off Platform
-        if(teamColor == Color.BLUE){
-            holonomic.run(-.5,0,0);
-            if(startingPosition == StartingPosition.LEFT){
-
-            }else if(startingPosition == StartingPosition.RIGHT){
-
-            }
-        } else if(teamColor == Color.RED){
-            holonomic.run(.5,0,0);
-            if(startingPosition == StartingPosition.LEFT){
-
-            }else if(startingPosition == StartingPosition.RIGHT){
-
-            }
-        }
-
-        //Rotate to line up to score
         float target = 0;
-        if(teamColor == Color.BLUE){
-            if(startingPosition == StartingPosition.LEFT){
-                switch(visibleVuMark){
-                    case LEFT:
-                        target = 0;
-                        if(target < 0){
-
-                        }else if(target > 0){
-
-                        }
-                        break;
-                    case CENTER:
-                        break;
-                    case RIGHT:
-                        break;
-                    case UNKNOWN:
-                    default:
-                        break;
-                }
-            }else if(startingPosition == StartingPosition.RIGHT){
-                switch(visibleVuMark){
-                    case LEFT:
-                        break;
-                    case CENTER:
-                        break;
-                    case RIGHT:
-                        break;
-                    case UNKNOWN:
-                    default:
-                        break;
-                }
-            }
-        } else if(teamColor == Color.RED){
-            if(startingPosition == StartingPosition.LEFT){
-                switch(visibleVuMark){
-                    case LEFT:
-                        break;
-                    case CENTER:
-                        break;
-                    case RIGHT:
-                        break;
-                    case UNKNOWN:
-                    default:
-                        break;
-                }
-            }else if(startingPosition == StartingPosition.RIGHT){
-                switch(visibleVuMark){
-                    case LEFT:
-                        break;
-                    case CENTER:
-                        break;
-                    case RIGHT:
-                        break;
-                    case UNKNOWN:
-                    default:
-                        break;
-                }
-            }
+        if(target < 0){
+            while(imuXAngle() > target && opModeIsActive())
+                holonomic.run(0,0,-.5);
+        }else if(target > 0){
+            while(imuXAngle() < target && opModeIsActive())
+                holonomic.run(0,0,.5);
         }
         holonomic.stop();
-        //Drive forward to score
-
-        //Release and Backup
-        leftIntake.setPosition(0);
-        rightIntake.setPosition(1);
-        sleep(1000);
 
 
 
